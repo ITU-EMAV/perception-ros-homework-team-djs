67b67909c0bda1b6826f79522d0dc53c35135a35
dweisbe <dweisbe000@citymail.cuny.edu>
2025-12-21 21:59:18 +0000

    Add 'tf_transformations' to install_requires

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'tf_transformations'],
     zip_safe=True,
     maintainer='emav',
     maintainer_email='emav@todo.todo',
